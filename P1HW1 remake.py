9078954a16b8ff27052a4d0d1b43cb32f44c3039
Iluvyang1 <130038614+Iluvyang1@users.noreply.github.com>
2023-05-10 11:40:37 -0400

    Add files via upload

--- P1HW1 remake.py
+++ P1HW1 remake.py
@@ -3,12 +3,18 @@
     # CTI-110 P1HW1 - Travel Expense
     # Javon Smith
 print("This program calculates and displays travel expenses")
-budget = float(input("Enter your budget : "))   # reading user budget
-destination = input("Enter your destination : ")    # reading user destination
-gas_expenses = float(input("How much you will spend on gas : "))    # reading user fuel expenses
-accomodation = float(input("Approximately how much you need for accomodation ? : "))    # reading user hostel expenses
-food_expenses = float(input("Last, how much do you need for food? : "))     # reading user food expenses
-remaining_balance = budget - gas_expenses - accomodation - food_expenses    # calculating remaining balance
+# enter user budget
+budget = float(input("Enter your budget : "))
+# enter user destination
+destination = input("Enter your destination : ")
+# enter user gas expenses
+gas_expenses = float(input("How much you will spend on gas : "))
+# enter users living expenses
+accomodation = float(input("Approximately how much you need for accomodation ? : "))
+# enter user food expenses
+food_expenses = float(input("Last, how much do you need for food? : "))
+# calculate remaining balance after other expenses
+remaining_balance = budget - gas_expenses - accomodation - food_expenses    
 print("------------------------------travel expenses----------------------------------")
 print("Location : " + destination)
 print("Initial budget : " + str(budget))
